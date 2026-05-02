14ebc15344b14b94c6c4d08b73588f09c2df402c
Mihai Nita <nmihai_2000@yahoo.com>
2026-05-02 10:53:25 -0700

    Update ICU to 78.3; mvn plugin versions, tomcat docker

--- icu4jweb/build-deploy.sh
+++ icu4jweb/build-deploy.sh
@@ -2,4 +2,4 @@
 set -x
 # the project id "icu4c-demos" cannot be changed.
 # Update the final part of the tag for each new release.
-docker build -t us-central1-docker.pkg.dev/goog-unicode-dev/unicode-jsps/icu4j-demos:74.1 .
+docker build -t us-central1-docker.pkg.dev/goog-unicode-dev/unicode-jsps/icu4j-demos:78.3 .
